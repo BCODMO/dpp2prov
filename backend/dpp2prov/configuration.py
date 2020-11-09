33203585badc5b03d97176d93a1b53fc77d95729
Adam Shepherd <ashepherd@whoi.edu>
2020-11-09 08:13:28 -0500

    Update configuration.py

--- backend/dpp2prov/configuration.py
+++ backend/dpp2prov/configuration.py
@@ -3,6 +3,8 @@ import os
 ENVIRONMENT_VARIABLE_NAME = 'ENVIRONMENT'
 BUCKET_VARIABLE_NAME = 'BUCKET'
 
-# {ENVIRONMENT}_{BUCKET} = the bucket to looks for FDP DPP YAML files
-def get_dpp2prov_bucket_name(environment=None):
-    return return os.getenv(os.getenv(ENVIRONMENT_VARIABLE_NAME) + '_' + BUCKET_VARIABLE_NAME)
+def get_environment():
+    return os.getenv(ENVIRONMENT_VARIABLE_NAME)
+
+def get_dpp2prov_bucket_name():
+    return return os.getenv(BUCKET_VARIABLE_NAME)
