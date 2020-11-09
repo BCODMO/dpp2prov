110ecfc16cd1e78b2f6bf533ae80ce87c73c898c
Adam Shepherd <ashepherd@whoi.edu>
2020-11-09 11:35:23 -0500

    Update configuration.py

--- backend/dpp2prov/configuration.py
+++ backend/dpp2prov/configuration.py
@@ -2,9 +2,13 @@ import os
 
 ENVIRONMENT_VARIABLE_NAME = 'ENVIRONMENT'
 BUCKET_VARIABLE_NAME = 'BUCKET'
+BCODMO_OFFICE_URI_VARIABLE_NAME = 'BCODMO_OFFICE_URI'
 
 def get_environment():
     return os.getenv(ENVIRONMENT_VARIABLE_NAME)
 
 def get_dpp2prov_bucket_name():
     return return os.getenv(BUCKET_VARIABLE_NAME)
+
+def get_bcodmo_office_uri():
+    return return os.getenv(BCODMO_OFFICE_URI_VARIABLE_NAME)
